3f4760ef9f23639af789afe0925a45bed5d30392
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-18 20:15:15 +0100

    feat: middlewares to hydrate, ensure_admin and is_verified fn

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,27 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import UserModel from "../core/user/models/user.model";
+import { adminRole } from "../utils/constants";
+import { responseKey, userKey } from "../core/responseKey";
 const { auth0 } = require('../config/config')
 const { auth } = require('express-oauth2-jwt-bearer');
 
-export const ensureAuth = auth({
+export const auth_0 = auth({
   audience: auth0.audience,
   issuerBaseURL: auth0.issuer,
   tokenSigningAlg: 'RS256'
-  // audience: 'http://localhost:4010',
-  // issuerBaseURL: 'https://dev-efues4oz5a3ih7sv.us.auth0.com/',
-  // tokenSigningAlg: 'RS256'
 });
 
-// const { expressjwt: jwt} = require('express-jwt')
-// const jwksRsa = require('jwks-rsa')
+export const hydrate = async (req: Request | any, res: Response, next: NextFunction) => {
+  const findUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub })
+  if (!findUser) {
+    return res.status(404).send({ message: userKey.notFound, key: 'hydrate' })
+  }
+  req.user = findUser
+  next()
+}
 
-// export const ensureAuth = jwt({
-//   secret: jwksRsa.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: `${auth0.issuer}.well-known/jwks.json`
-//   }),
+export const ensure_admin = async (req: Request | any, res: Response, next: NextFunction) => {
+  const findUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub })
+  if (!findUser) {
+    return res.status(404).send({ message: userKey.notFound, key: 'ensure_admin' })
+  }
+  if (findUser.role !== adminRole) {
+    return res.status(401).send({ message: responseKey.unauthorized })
+  }
+  if (findUser.isVerified === false) {
+    return res.status(401).send({ message: userKey.notVerified })
+  }
+  req.user = findUser
+  next()
+}
 
-//   audience: auth0.audience,
-//   issuer: auth0.issuer,
-//   algorithms: ['RS256']
-// })
+export const is_verified = async (req: Request | any, res: Response, next: NextFunction) => {
+  const findUser = await UserModel.findOne({ auth0Id: req.auth.payload.sub })
+  if (!findUser) {
+    return res.status(404).send({ message: userKey.notFound, key: 'is_verified' })
+  }
+  if (findUser.isVerified === false) {
+    return res.status(401).send({ message: userKey.notVerified })
+  }
+  req.user = findUser
+  next()
+}
