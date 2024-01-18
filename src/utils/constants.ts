5926ce1a78f3b5c2a0d6317eaed366763c8d432b
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-18 20:14:12 +0100

    feat: roles and environment contants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const adminRole = 'admin'
+export const creatorRole = 'creator'
+export const userRole = 'user'
+export const developmentStage = 'development'
+export const productionStage = 'production'
