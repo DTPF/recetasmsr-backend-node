322c7a4018abce08349cdce6aba053b420f2576f
David T. Pizarro Frick <davidpizarrofrick@gmail.com>
2024-01-18 19:47:26 +0100

    change: add client url and variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,20 @@
+import { developmentStage, productionStage } from "../utils/constants"
 const dotenv = require('dotenv')
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === productionStage) {
   dotenv.config({ path: '.env.production' })
 } else {
   dotenv.config({ path: '.env.development' })
 }
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || developmentStage
 
 export type ServerConfig = {
   app: {
     URL: string,
     PORT: string | number,
     API_VERSION: string,
+    CLIENT_URL: string
   },
   db: {
     MONGO_URL: string,
@@ -35,6 +37,7 @@ const CONFIG: ConfigEnv = {
       URL: `http://${process.env.IP_SERVER}:${process.env.PORT_SERVER}/api/${process.env.API_VERSION}`,
       PORT: process.env.PORT_SERVER || 4000,
       API_VERSION: process.env.API_VERSION || 'v1',
+      CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000'
     },
     db: {
       MONGO_URL: `mongodb://${process.env.IP_SERVER}:${process.env.PORT_MONGO_DB}/${process.env.DB_NAME}`,
@@ -43,7 +46,7 @@ const CONFIG: ConfigEnv = {
     auth0: {
       audience: process.env.AUTH0_AUDIENCE,
       issuer: process.env.AUTH0_ISSUER,
-      issuerToken: process.env.AUTH0_ISSUER
+      issuerToken: process.env.AUTH0_ISSUER_TOKEN
     },
   },
   production: {
@@ -51,6 +54,7 @@ const CONFIG: ConfigEnv = {
       URL: `http://${process.env.IP_SERVER}${process.env.PORT_SERVER}/api/${process.env.API_VERSION}`,
       PORT: process.env.PORT_SERVER || 4006,
       API_VERSION: process.env.API_VERSION || 'v1',
+      CLIENT_URL: process.env.CLIENT_URL || 'http://localhost:3000'
     },
     db: {
       MONGO_URL: `mongodb://${process.env.DB_USER_PASSWORD}@${process.env.IP_SERVER}:${process.env.PORT_MONGO_DB}/${process.env.DB_NAME}?authSource=admin`,
